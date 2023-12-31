b96485d34a9d24c0afb1733f10a7cbdc739d19e6
brianmstricker <brianmstricker@gmail.com>
2023-12-31 00:09:16 -0500

    feat: theme toggle button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
 
 export const fontSans = FontSans({
  subsets: ["latin"],
@@ -26,7 +28,10 @@ export default function RootLayout({
      fontSans.variable
     )}
    >
-    {children}
+    <ThemeProvider defaultTheme="dark">
+     <Navbar />
+     {children}
+    </ThemeProvider>
    </body>
   </html>
  );
---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useTheme } from "next-themes";
+import { FaRegSun } from "react-icons/fa6";
+import { LuMoon } from "react-icons/lu";
+import { useEffect, useState } from "react";
+
+export function ThemeToggleButton() {
+ const [mounted, setMounted] = useState(false);
+ const { setTheme, theme } = useTheme();
+ useEffect(() => setMounted(true), []);
+ function changeTheme() {
+  if (theme === "dark") {
+   setTheme("light");
+  } else {
+   setTheme("dark");
+  }
+ }
+ if (!mounted) return <div className="w-6 h-6 bg-gray-400/20 rounded-full" />;
+ return (
+  <button onClick={changeTheme}>
+   {theme === "dark" ? (
+    <LuMoon className="w-6 h-6" />
+   ) : (
+    <FaRegSun className="w-6 h-6" />
+   )}
+   <span className="sr-only">Toggle theme</span>
+  </button>
+ );
+}
