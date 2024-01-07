eb8d8ccf5913949f7f6a9f5dca1fbb31c020c7e3
brianmstricker <brianmstricker@gmail.com>
2024-01-07 00:01:18 -0500

    feat: middleware/protected routes

---
+++ src/routes.ts
@@ -0,0 +1,4 @@
+export const publicRoutes = ["/", "/home"];
+export const authRoutes = ["/auth", "/auth/login", "/auth/signup"];
+export const apiAuthPrefix = "/api/auth";
+export const DEFAULT_LOGIN_REDIRECT = "/home";
