aaa3e9de8076db6de1c37a085babe25565cdd798
brianmstricker <brianmstricker@gmail.com>
2024-01-23 20:32:51 -0500

    feat: display image useEffect based on height/width/aspect ratio

--- src/components/Home/DisplayTweetMediaModal.tsx
+++ src/components/Home/DisplayTweetMediaModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import HideScroll from "../HideScroll";
 import FocusTrap from "focus-trap-react";
@@ -21,11 +21,15 @@ const DisplayTweetMediaModal = ({
  media,
  closeModal,
 }: DisplayTweetMediaModalProps) => {
- const modalRef = useRef<HTMLDivElement | null>(null);
+ const [scaledDimensions, setScaledDimensions] = useState({
+  width: 0,
+  height: 0,
+ });
+ const imgRef = useRef<HTMLDivElement | null>(null);
  const menuRef = useRef<HTMLDivElement | null>(null);
  useEffect(() => {
   const handleOutsideClick = (e: any) => {
-   const modalNode = modalRef.current;
+   const modalNode = imgRef.current;
    const menuNode = menuRef.current;
    if (
     modalNode &&
@@ -40,20 +44,45 @@ const DisplayTweetMediaModal = ({
   return () => {
    document.removeEventListener("mousedown", handleOutsideClick);
   };
- }, [modalRef, closeModal]);
- // useEffect(() => {
- //  if (!modalShow) return;
- //  const handleKeyDown = (e: any) => {
- //   if (e.key === "Escape") {
- //    closeModal();
- //   }
- //  };
- //  window.addEventListener("keydown", handleKeyDown);
- //  return () => {
- //   window.removeEventListener("keydown", handleKeyDown);
- //  };
- // }, [closeModal, modalShow]);
- console.log(media.aspectRatio);
+ }, [closeModal]);
+ useEffect(() => {
+  const handleKeyDown = (e: any) => {
+   if (e.key === "Escape") {
+    closeModal();
+   }
+  };
+  window.addEventListener("keydown", handleKeyDown);
+  return () => {
+   window.removeEventListener("keydown", handleKeyDown);
+  };
+ }, [closeModal]);
+ const calculateScaledDimensions = useCallback(() => {
+  const maxWidth = window.innerWidth;
+  const maxHeight = window.innerHeight;
+  const aspectRatio = Number(media.aspectRatio);
+  let width, height;
+  if (aspectRatio > 1) {
+   width = maxWidth;
+   height = maxWidth / aspectRatio;
+  } else {
+   height = maxHeight;
+   width = maxHeight * aspectRatio;
+  }
+  width = Math.min(width, Number(media.width));
+  height = Math.min(height, Number(media.height));
+  return { width, height };
+ }, [media.aspectRatio, media.width, media.height]);
+ useEffect(() => {
+  const updateDimensions = () => {
+   const newDimensions = calculateScaledDimensions();
+   setScaledDimensions(newDimensions);
+  };
+  updateDimensions();
+  window.addEventListener("resize", updateDimensions);
+  return () => {
+   window.removeEventListener("resize", updateDimensions);
+  };
+ }, [media.aspectRatio, calculateScaledDimensions]);
  return createPortal(
   <>
    <HideScroll>
@@ -66,32 +95,28 @@ const DisplayTweetMediaModal = ({
       className="w-screen h-screen fixed inset-0 bg-white/95 dark:bg-black/95 z-[100]"
      >
       <div className="flex h-full">
-       <div className="w-full h-full flex justify-center">
-        <div
-         ref={modalRef}
-         className="-top-2 relative max-h-[95%] max-w-[95%] mx-auto"
-         style={{ aspectRatio: Number(media.aspectRatio) }}
-        >
-         <div className="relative w-full h-full">
+       <div className="w-full h-full relative flex flex-col overflow-hidden">
+        <div className="flex-1 flex items-center justify-center">
+         <div ref={imgRef}>
           <Image
            src={media.url}
            alt="tweet media"
-           className="object-cover"
-           fill
+           className="mx-auto max-w-full max-h-full"
+           style={{ aspectRatio: Number(media.aspectRatio) }}
+           width={scaledDimensions.width}
+           height={scaledDimensions.height}
           />
          </div>
-         <div className="w-fit mx-auto">
-          <div className="mx-auto" tabIndex={0}>
-           like/favorite/etc
-          </div>
-         </div>
+        </div>
+        <div className="w-full border">
+         <div tabIndex={0}>like/favorite/etc</div>
         </div>
        </div>
        <div
         ref={menuRef}
-        className="h-full bg-black ml-auto border-l dark:border-l-white/25"
+        className="h-full bg-black ml-auto border-l dark:border-l-white/25 shrink-0 min-w-[330px] max-w-[330px]"
        >
-        <div className="p-2 max-w-[330px]">right menu</div>
+        <div className="p-2">right menu here</div>
        </div>
       </div>
      </div>
