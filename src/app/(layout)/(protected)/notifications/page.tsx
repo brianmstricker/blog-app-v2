34c5f6805975777c7ea516eb0c98e7da9fc2fcbc
brianmstricker <brianmstricker@gmail.com>
2024-02-20 22:30:31 -0500

    feat: notification page

--- src/app/(layout)/(protected)/notifications/page.tsx
+++ src/app/(layout)/(protected)/notifications/page.tsx
@@ -1,4 +1,19 @@
 const page = () => {
- return <div>page</div>;
+ return (
+  <div tabIndex={0}>
+   <h1 className="text-xl font-medium pt-3 px-4">Notifications</h1>
+   <div className="w-full h-0.5 my-3 border-b dark:border-white/25" />
+   <div className="max-w-sm mx-auto mt-4">
+    <p className="text-4xl font-bold">
+     Nothing to see here — <br />
+     yet
+    </p>
+    <p className="text-[16px] text-mainGray mt-2 tracking-tight">
+     From likes to reposts and a whole lot more, this is where all the action
+     happens.
+    </p>
+   </div>
+  </div>
+ );
 };
 export default page;
