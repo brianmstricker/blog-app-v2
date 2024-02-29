86cb0933207f29fdb34971cf0027914ea8e29038
brian <brianmstricker@gmail.com>
2024-02-29 00:52:08 -0500

    update: middleware public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,7 +13,10 @@ export default auth((req) => {
  const { nextUrl } = req;
  const isLoggedIn = !!req.auth;
  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
- const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+ const isPublicRoute =
+  publicRoutes.includes(nextUrl.pathname) ||
+  nextUrl.pathname.match(/\/status\/.*/) ||
+  nextUrl.pathname.match(/\/user\/.*/);
  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
  if (isApiAuthRoute) return null;
  if (isAuthRoute) {
